fbb5a493b02fc8978aa0a85e8ee243edb0417646
Alvin Li <hins.xd@hotmail.com>
2020-10-22 01:05:12 +0800

    Implement graphql server.

---
+++ server/index.ts
@@ -0,0 +1,81 @@
+import { NewsItem } from 'common/types';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+import newsItems from '../data.json';
+
+const schema = buildSchema(/* GraphQL */ `
+  enum OrderBy {
+    comments
+    points
+  }
+
+  enum Order {
+    desc
+    asc
+  }
+
+  type NewsItem {
+    id: Int!
+    title: String!
+    points: Int!
+    author: String
+    comments: Int!
+    link: String
+  }
+
+  type NewsItemsPayload {
+    count: Int!
+    newsItems: [NewsItem!]!
+  }
+
+  type Query {
+    items(
+      orderBy: OrderBy = comments
+      order: Order = desc
+      limit: Int = 10
+      offset: Int = 0
+    ): NewsItemsPayload!
+  }
+`);
+
+// Resolver
+type ItemsArgs = {
+  orderBy: 'points' | 'comments';
+  order: 'desc' | 'asc';
+  limit: number;
+  offset: number;
+};
+type ItemsPayload = {
+  count: number;
+  newsItems: NewsItem[];
+};
+const root = {
+  items: (args: ItemsArgs): ItemsPayload => {
+    const { limit, offset, order, orderBy } = args;
+
+    // If desc order, sort comparator is inverted.
+    const modifier = order === 'desc' ? -1 : 1;
+    const result = [...newsItems]
+      .sort((a, b) => modifier * (a[orderBy] - b[orderBy]))
+      .slice(offset, offset + limit);
+
+    return {
+      count: result.length,
+      newsItems: result,
+    };
+  },
+};
+
+const app = express();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+});
