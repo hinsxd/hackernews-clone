0ce5feab39d9a46b2c383f286d10fd8592a23254
Alvin Li <hins.xd@hotmail.com>
2020-10-22 03:30:09 +0800

    User slower but reliable way to scrape all items. Fix type missing: 'time'.

--- common/types.ts
+++ common/types.ts
@@ -5,6 +5,7 @@ export type NewsItem = {
   author: string | null;
   comments: number;
   link: string | null;
+  time: string;
 };
 
 export type ItemsArgs = {
--- scraper/index.ts
+++ scraper/index.ts
@@ -10,56 +10,68 @@ async function loadPage(page = 1): Promise<string> {
   return axios.get(`${URL}${page}`).then((result) => result.data);
 }
 
-async function getNewsItems(page = 1): Promise<NewsItem[]> {
-  const html = await loadPage(page);
-  const $ = cheerio.load(html);
+async function getNewsItems(): Promise<NewsItem[]> {
+  const newsItems: NewsItem[] = [];
+  // HN support only 4 parallel requests, and number of posts are not infinite.
+  // Only about 14 pages currently.
 
-  // Remove distracting spacer
-  const spacer = $('body').find('table.itemlist tr.spacer');
-  spacer.remove();
+  // Using traditional way to see if having next page.
+  // If still has next page, load and parse next page.
+  let page = 1;
+  let hasNext = true;
+  while (hasNext) {
+    console.log('processing page', page);
+    const html = await loadPage(page);
+    const $ = cheerio.load(html);
 
-  const titleRows = $('table.itemlist tbody tr.athing');
+    // Remove distracting spacer
+    const spacer = $('body').find('table.itemlist tr.spacer');
+    spacer.remove();
 
-  const items: NewsItem[] = titleRows.toArray().map((el) => {
-    const id = +el.attribs.id;
-    // Title and link
-    const titleEl = $(`tr#${id}.athing > td.title`);
-    const storylinkEl = titleEl.find('a.storylink');
-    const title = storylinkEl.text();
-    const link = storylinkEl.attr().href;
+    const titleRows = $('table.itemlist tbody tr.athing');
 
-    // Point
-    const pointEl = $(`#score_${id}`);
-    const pointStr = pointEl.text(); // Sometimes there will be no points
-    const points = pointStr === '' ? 0 : +pointStr.split(' ')[0]; // 'n points' -> n
-    // author name
-    const author = pointEl.siblings('a.hnuser').text() || null;
+    titleRows.toArray().forEach((el) => {
+      const id = +el.attribs.id;
+      // Title and link
+      const titleEl = $(`tr#${id}.athing > td.title`);
+      const storylinkEl = titleEl.find('a.storylink');
+      const title = storylinkEl.text();
+      const link = storylinkEl.attr().href;
 
-    // comments count
-    const commentEl = pointEl.siblings(`a[href^="item?"]`);
-    const commentStr = commentEl.text();
-    // For some unknown reason, commentStr.split(' ') is not working.
-    // Using regex instead
-    const countMatch = /^(\d+).*comment/.exec(commentStr);
-    const comments = countMatch ? parseInt(countMatch[0]) : 0;
+      // Point
+      const pointEl = $(`#score_${id}`);
+      const pointStr = pointEl.text(); // Sometimes there will be no points
+      const points = pointStr === '' ? 0 : +pointStr.split(' ')[0]; // 'n points' -> n
+      // author name
+      const author = pointEl.siblings('a.hnuser').text() || null;
 
-    // Parse post date string with 'chrono-node'
-    const timeStr = pointEl.siblings('.age').find('a').text();
-    const time = parse(timeStr)[0]?.date().toISOString() || null;
-    return { id, points, author, title, link, comments, time };
-  });
+      // comments count
+      const commentEl = pointEl.siblings(`a[href^="item?"]`);
+      const commentStr = commentEl.text();
+      // For some unknown reason, commentStr.split(' ') is not working.
+      // Using regex instead
+      const countMatch = /^(\d+).*comment/.exec(commentStr);
+      const comments = countMatch ? parseInt(countMatch[0]) : 0;
 
-  return items;
+      // Parse post date string with 'chrono-node'
+      const timeStr = pointEl.siblings('.age').find('a').text();
+      const time = parse(timeStr)[0]?.date().toISOString() || null;
+
+      newsItems.push({ id, points, author, title, link, comments, time });
+    });
+
+    // Look for 'More' button
+    if ($('.morelink').get().length > 0) {
+      page++;
+    } else {
+      hasNext = false;
+    }
+  }
+  return newsItems;
 }
 export default async function scrape() {
-  // For convenience, get 4 pages in parallel, because HN does not support 5 parallel requests.
-  const newsItems = await Promise.all([1, 2, 3, 4].map(getNewsItems)).then(
-    // An array of 4 arrays of news items
-    (newsArrArr) =>
-      // Flatten to one big array
-      newsArrArr.reduce((flattened, newsArr) => [...flattened, ...newsArr], [])
-  );
-  fs.writeFileSync('data.json', JSON.stringify(newsItems, null, 2));
+  const items = await getNewsItems();
+  fs.writeFileSync('data.json', JSON.stringify(items, null, 2));
 }
 
 scrape();
